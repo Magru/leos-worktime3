eaba6d1424386cb8c8e60e61e323cb8837809398
Maxim Folko <maxf@leos.co.il>
2022-06-16 09:34:03 +0300

    args

--- application/app/Http/Controllers/ClockController.php
+++ application/app/Http/Controllers/ClockController.php
@@ -129,7 +129,7 @@ class ClockController extends Controller
 
                 if(!$allow_early_in->free_in){
 
-                    if(!(strtotime(date('H:i')) > config('app.in_hour'))){
+                    if(!(strtotime(date('H:i')) < config('app.in_hour'))){
                         return response()->json([
                             "error" => 'אין אפשרות לעשות כניסה לפני ' . config('app.in_hour_text'),
                         ]);
