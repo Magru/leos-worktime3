821aa2d762373bba1082d11aee222248b81963e2
Maxim Folko <maxf@leos.co.il>
2022-07-04 12:30:03 +0300

    test

--- application/app/Http/Controllers/ClockController.php
+++ application/app/Http/Controllers/ClockController.php
@@ -125,8 +125,8 @@ class ClockController extends Controller
                     $in_hour = config('app.in_hour');
                     $now = Carbon::now();
 
-                    var_dump($now->toDateString());
-                    var_dump($in_hour->toDateString());
+                    var_dump($now->toDateTimeString());
+                    var_dump($in_hour->toDateTimeString());
                     var_dump($now->greaterThan($in_hour));
 
                     if ($now->greaterThan($in_hour)) {
